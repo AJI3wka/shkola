7c60a138f049eae22bbb9c7c267c8e61b50858c1
AJI3wka <lexapryhoda@gmail.com>
2018-12-13 05:17:43 +0300

    adaptiv

--- Gruntfile.js
+++ Gruntfile.js
@@ -620,7 +620,7 @@ module.exports = function(grunt) {
                     spawn: true,
                     livereload: {
                         host: 'localhost',
-                        port: 6659, //LIVERELOAD_PORT
+                        port: 2054, //LIVERELOAD_PORT
                     }
                 },
                 //при изменеии любых сорс файлов
